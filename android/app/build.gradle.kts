d4382051cd9447e0b49965b8f2af4732e2c103f4
coderchessansh <anshuman21das@gmail.com>
2026-08-11 22:19:26 +0530

    Fix Android JVM target mismatch

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -16,10 +16,10 @@ android {
         sourceCompatibility = JavaVersion.VERSION_17
         targetCompatibility = JavaVersion.VERSION_17
     }
-}
 
-kotlin {
-    jvmToolchain(17)
+    kotlinOptions {
+        jvmTarget = "17"
+    }
 }
 
 dependencies {
